8a070be0cb0b9b0ab56e8329c7c6f4ae4a602404
elvania01 <190442305+elvania01@users.noreply.github.com>
2025-04-22 14:24:20 +0700

    Update page.tsx

--- app/categories/birthday/page.tsx
+++ app/categories/birthday/page.tsx
@@ -19,7 +19,7 @@ export default function SpecialEventCategory() {
 
   return (
     <div className="p-8">
-      <h1 className="font-pacifico text-3xl font-bold text-pink-600 mb-6">Special Event Collection</h1>
+      <h1 className="font-pacifico text-3xl font-bold text-pink-600 mb-6">Birthday Collectionpnpm approve-builds Collection</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map(product => (
           <div key={product.id} className="bg-white rounded-xl shadow p-4 text-center">
