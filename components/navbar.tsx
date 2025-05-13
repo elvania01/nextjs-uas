c4330ece081da7de66bd3af697e81db5017e1cdf
elvania01 <elvaniapranisti1@gmail.com>
2025-05-13 21:11:47 +0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -108,8 +108,9 @@ export default function Navbar() {
       <div className="font-pacifico max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         {/* Left Navigation */}
         <div className="flex items-center space-x-6 relative">
-          <Link href="/about-us" className="hover:bg-pink-600 px-2 py-1 rounded-md">About Us</Link>
+          <Link href="/analytic" className="hover:bg-pink-600 px-2 py-1 rounded-md">Dashboard</Link>
           <Link href="/" className="hover:bg-pink-600 px-2 py-1 rounded-md">Home</Link>
+          <Link href="/about-us" className="hover:bg-pink-600 px-2 py-1 rounded-md">About Us</Link>
           <Link href="/testimoni" className="hover:bg-pink-600 px-2 py-1 rounded-md">Testimoni</Link>
           {role === "Owner" && (
             <Link href="/transaction" className="hover:bg-pink-600 px-2 py-1 rounded-md">
